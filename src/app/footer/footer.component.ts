5bd915b7f88125cae42689455e2521677b82ce1a
ahron-maslin <aronmaslin@gmail.com>
2022-02-04 00:24:49 +0200

    back

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { faHome, faInfo, faShoppingBag, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  faHome = faHome;
+  faInfo = faInfo;
+  faShop = faShoppingBag;
+  faAddress = faAddressCard;
+
+}
