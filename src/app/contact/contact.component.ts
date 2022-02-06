8b1bb56515289d8c14b2344f525c176856b95d74
ahron-maslin <aronmaslin@gmail.com>
2022-02-06 22:50:57 +0200

    new form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,8 +15,7 @@ export class ContactComponent implements OnInit{
   faMailBulk = faMailBulk;
   faPhoneSquare = faPhoneSquare;
   form: FormGroup;
-  firstname: FormControl = new FormControl("", [Validators.required]);
-  lastname: FormControl = new FormControl("", [Validators.required]);
+  name: FormControl = new FormControl("", [Validators.required]);
   email: FormControl = new FormControl("", [Validators.required, Validators.email]);
   message: FormControl = new FormControl("", [Validators.required, Validators.maxLength(256)]);
   honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
@@ -25,8 +24,7 @@ export class ContactComponent implements OnInit{
   responseMessage: string; // the response message to show to the user
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {
     this.form = this.formBuilder.group({
-      firstname: this.firstname,
-      lastname: this.lastname,
+      name: this.name,
       email: this.email,
       message: this.message,
       honeypot: this.honeypot
@@ -38,24 +36,18 @@ export class ContactComponent implements OnInit{
     if (this.form.status == "VALID" && this.honeypot.value == "") {
       this.form.disable(); // disable the form if it's valid to disable multiple submissions
       var formData: any = new FormData();
-      formData.append("firstname", this.form.get('firstname')?.value);
-      formData.append("lastname", this.form.get('lastname')?.value);
+      formData.append("name", this.form.get("name")?.value);
       formData.append("email", this.form.get("email")?.value);
       formData.append("message", this.form.get("message")?.value);
       this.isLoading = true; // sending the post request async so it's in progress
       this.submitted = false; // hide the response message on multiple submits
-      this.http.post("YOUR GOOGLE WEB APP URL HERE", formData).subscribe(
+      this.http.post("https://formspree.io/f/xjvlyjod", formData).subscribe(
         (response) => {
-          // choose the response message
-          /*if (response["result"] == "success") {
-            this.responseMessage = "Thanks for the message! I'll get back to you soon!";
-          } else {
-            this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
-          }*/
           this.form.enable(); // re enable the form after a success
           this.submitted = true; // show the response message
           this.isLoading = false; // re enable the submit button
           console.log(response);
+          this.responseMessage = this.form.get("name")?.value;
         },
         (error) => {
           this.responseMessage = "Oops! An error occurred... Reload the page and try again.";
