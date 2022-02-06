8cfcb76a7e63f474e5c85503e38fb21693bbf1fd
ahron-maslin <aronmaslin@gmail.com>
2022-02-06 22:25:38 +0200

    asdf

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -38,19 +38,20 @@ export class ContactComponent implements OnInit{
     if (this.form.status == "VALID" && this.honeypot.value == "") {
       this.form.disable(); // disable the form if it's valid to disable multiple submissions
       var formData: any = new FormData();
-      formData.append("name", this.form.get("name").value);
-      formData.append("email", this.form.get("email").value);
-      formData.append("message", this.form.get("message").value);
+      formData.append("firstname", this.form.get('firstname')?.value);
+      formData.append("lastname", this.form.get('lastname')?.value);
+      formData.append("email", this.form.get("email")?.value);
+      formData.append("message", this.form.get("message")?.value);
       this.isLoading = true; // sending the post request async so it's in progress
       this.submitted = false; // hide the response message on multiple submits
       this.http.post("YOUR GOOGLE WEB APP URL HERE", formData).subscribe(
         (response) => {
           // choose the response message
-          if (response["result"] == "success") {
+          /*if (response["result"] == "success") {
             this.responseMessage = "Thanks for the message! I'll get back to you soon!";
           } else {
             this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
-          }
+          }*/
           this.form.enable(); // re enable the form after a success
           this.submitted = true; // show the response message
           this.isLoading = false; // re enable the submit button
