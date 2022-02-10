653d76cdc001abd97585dbfb6daf658ed8bacd3d
ahron-maslin <aronmaslin@gmail.com>
2022-02-11 01:19:27 +0200

    about me update

--- src/app/shared/about.ts
+++ src/app/shared/about.ts
@@ -15,7 +15,11 @@ export const ABOUT: ABOUT_SCHEMA[] = [
         "title": "Steve the Aquatic Punchball",
         "image": "../assets/images/Steve.jpeg",
         "alt": "Steve the Aquatic Punchball",
-        "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "text":`It's hard to imagine what life would be like without me. I am the ray of light shining in our dark times. When I first woke up in that dark alleyway and saw those goons getting ready to ship me off who-knows-where, I thought I was gone for.
+            And then Simcha appeared, Simcha Nierenberg, the legend himself! I don't recall much, but I can tell you that the fight was very brief. Those guys didn't stand a chance. Flash forward five years in the future (currently the present), and once again I stand at an emotional dilemma. Although not as scary as the alleyway,
+            this matter is much more pressing. I need to provide for a family now, pay the bills, etc. Society is just as unforgiving as those goons - although it does it with a freindly smile and the misconception that money is free and lasts forever. But once again Simcha appeared and offered to help me out. "I have a solution to your problems", he said, 
+            although this time there would be a price. I would have to sell me body and soul - become a "meme" as they call it nowadays. Was I ready to face the world? I was desperate for the support.
+            But the real question was, is the world ready to face me? I'll let you ponder that as you warm yourself in a your new sweater or whatever else you decide to buy.`,
         "footer": ""
 
     },
@@ -25,7 +29,7 @@ export const ABOUT: ABOUT_SCHEMA[] = [
         "title": "Simcha CEO/Founder",
         "image": "/assets/images/Simcha.jpg",
         "alt": "Simcha CEO/Founder",
-        "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "text":`writers block`,
         "footer": ""
 
     },
@@ -35,7 +39,12 @@ export const ABOUT: ABOUT_SCHEMA[] = [
         "title": "Our Vision",
         "image": "/assets/images/vision.jpg",
         "alt": "Our Vision",
-        "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "text":`Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum.`,
         "footer": ""
 
     }
